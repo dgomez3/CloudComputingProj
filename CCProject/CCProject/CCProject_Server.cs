fd9377a26ec381014326f4bbabecd79bd53c1ef0
dgomez3 <dgomez3@nd.edu>
2013-09-17 21:58:51 -0400

    Added the project to GitHub.

---
+++ CCProject/CCProject/CCProject_Server.cs
@@ -0,0 +1,241 @@
+﻿/*
+ * Dan Gomez
+ * 
+ * A simple server that stores basic financial transactions for personal
+ * use. It uses Isis so that multiple servers and clients can share data
+ * and maintain accurate records.
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetworkCommsDotNet;
+using Isis;
+
+namespace CCProject
+{
+
+    // The finance book class stores transactions by a key-value pair.
+    // It also contains functions to handle the incoming and outgoing
+    // messages to and from the client. 
+    //
+    class FinanceBook
+    {
+        // Stores financial info
+        // Key - note about the transaction
+        // Value - amount withdrawn/deposited
+        Dictionary<string, float> book;
+        float balance;
+        private Group isisGroup;
+        private static int UPDATE = 0;
+        private static int QUERY = 1;
+        private static int BALANCE = 2;
+        private static int ALL = 3;
+        private delegate void IsisUpdateDelegate(string info, float money);
+        private delegate void IsisQueryDelegate(string info);
+        private delegate void IsisBalanceDelegate();
+        private delegate void IsisAllDelegate();
+
+        delegate void loadcheckpoint(string info, float money);
+
+        // Simply add and entry.
+        //
+        public void IsisUpdate(string info, float money)
+        {
+            Console.WriteLine("Update " + info + ":" + money);
+            addEntry(info, money);
+        }
+
+        // Respond to a query.
+        //
+        public void IsisQuery(string info)
+        {
+            Console.WriteLine("Query " + info);
+            if (book.ContainsKey(info))
+            {
+                isisGroup.Reply(info);
+            }
+            else isisGroup.Reply("");
+        }
+
+        // Respond with the balance.
+        //
+        public void IsisBalance()
+        {
+            Console.WriteLine("Balance " + balance);
+            isisGroup.Reply(balance);
+        }
+
+        // Respond with the entire finance book.
+        //
+        public void IsisAll()
+        {
+            Console.WriteLine("All");
+            isisGroup.Reply(book);
+        }
+
+        // Simple Constructor.
+        //
+        public FinanceBook()
+        {
+            book = new Dictionary<string, float>();
+            balance = 0;
+        }
+
+        // Start the server. 
+        //
+        public void Start(string ip, int port)
+        {
+            System.Net.IPEndPoint endpoint = new System.Net.IPEndPoint(System.Net.IPAddress.Parse(ip), port);
+            TCPConnection.StartListening(endpoint);
+
+            // Packet handlers for the four types of messages we are expecting.
+            //
+            NetworkComms.AppendGlobalIncomingPacketHandler<string>("Get", ClientGet);
+            NetworkComms.AppendGlobalIncomingPacketHandler<string>("Add", ClientUpdate);
+            NetworkComms.AppendGlobalIncomingPacketHandler<string>("Bal", ClientBalance);
+            NetworkComms.AppendGlobalIncomingPacketHandler<string>("All", ClientAll);
+
+            isisGroup = new Group("book");
+            IsisUpdateDelegate func1 = IsisUpdate;
+            isisGroup.Handlers[UPDATE] += func1;
+            IsisQueryDelegate func2 = IsisQuery;
+            isisGroup.Handlers[QUERY] += func2;
+            IsisBalanceDelegate func3 = IsisBalance;
+            isisGroup.Handlers[BALANCE] += func3;
+            IsisAllDelegate func4 = IsisAll;
+            isisGroup.Handlers[ALL] += func4;
+
+            // Create and load checkpoints to keep multiple books up to date with the same data.
+            //
+            isisGroup.MakeChkpt += (Isis.ChkptMaker)delegate(View nv)
+            {
+                Console.WriteLine("Sending checkpoint!");
+                foreach (KeyValuePair<string, float> pair in book)
+                    isisGroup.SendChkpt(pair.Key, pair.Value);
+                isisGroup.EndOfChkpt();
+            };
+
+            isisGroup.LoadChkpt += (loadcheckpoint)delegate(string info, float money)
+            {
+                Console.WriteLine("Checkpoint recieved!" + info + ":" + money);
+                addEntry(info, money);
+            };
+
+            byte[] key = new byte[32];
+            for (int i = 0; i < 32; i++) key[i] = (byte)i;
+            isisGroup.SetSecure(key);
+
+            isisGroup.Join();
+        }
+
+        // Add an entry to the finance book and update the balance.
+        //
+        private void addEntry(string info, float money)
+        {
+            if (book.ContainsKey(info))
+            {
+                balance -= book[info];
+                book.Remove(info);
+            }
+            book.Add(info, money);
+            balance += money;
+        }
+
+        // Query the fianance book (and the book for other servers if necessary) when the user
+        // looks up a certain entry.
+        //
+        private void ClientGet(PacketHeader header, Connection connection, string info)
+        {
+            Console.WriteLine("Querying");
+            if (book.ContainsKey(info))
+            {
+                Console.WriteLine("Found it!");
+                connection.SendObject("item", book[info]);
+            }
+            else
+            {
+                Console.WriteLine("Querying");
+                List<string> replies = new List<string>();
+                int nreplies = isisGroup.Query(Group.ALL,
+                    new Timeout(1000,Timeout.TO_FAILURE),
+                    QUERY,
+                    info,
+                    new EOLMarker(), replies);
+                Console.WriteLine("Replies: " + nreplies);
+                foreach (string reply in replies)
+                {
+                    if (reply != null)
+                    {
+                        //addEntry(info, reply);
+                        connection.SendObject("item", reply);
+                        break;
+                    }
+                }
+                Console.WriteLine("shiku shiku!");
+                connection.SendObject("item", null);
+            }
+        }
+
+        // Handle messages to add entries to the finance book.
+        //
+        private void ClientUpdate(PacketHeader header, Connection connection, string itembook)
+        {
+            Console.WriteLine("Client update");
+            string info = itembook.Split(':').First();
+            string temp = itembook.Split(':').Last();
+            float money = float.Parse(temp);
+ 
+            addEntry(info, money);
+            isisGroup.OrderedSend(UPDATE, info, money);
+        }
+
+        // Send the balance to the client for the user.
+        //
+        private void ClientBalance(PacketHeader header, Connection connection, string foo)
+        {
+            Console.WriteLine("balance" + foo);
+            connection.SendObject("balance", balance);
+        }
+
+        // Send the finance book to the client for the user.
+        //
+        private void ClientAll(PacketHeader header, Connection connection, string foo)
+        {
+            Console.WriteLine("All" + foo);
+            connection.SendObject("all", book);
+        }
+
+        // Shuts the server down.
+        //
+        public void Stop()
+        {
+            NetworkComms.Shutdown();
+        }
+    }
+
+    // The actual server class. It uses Isis to manage outgoing and incoming messages from the
+    // client. 
+    //
+    class CCProject_Server
+    {
+        static void Main(string[] args)
+        {
+            string ip;
+            if (args.Length == 0) ip = "127.0.0.1";
+            else ip = args[0];
+
+            IsisSystem.Start();
+            FinanceBook foobar = new FinanceBook();
+            foobar.Start(ip, 20000);
+
+            Console.WriteLine("\nServer is up\n--To close the server, press any key--");
+            Console.ReadKey(true);
+            foobar.Stop();
+            IsisSystem.Shutdown();
+        }
+    }
+}
---
+++ CCProject/CCProject_Client/CCProject_Client.cs
@@ -0,0 +1,97 @@
+﻿/*
+ * Dan Gomez
+ * 
+ * The client side of the application. A simple user interface to prompt the user
+ * and send messages to the server.
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetworkCommsDotNet;
+
+namespace CCProject_Client
+{
+    class CCProject_Client
+    {
+        static void Main(string[] args)
+        {
+
+            // Connect to the server based on the info given (or by default).
+            //
+            string ServerInfo;
+            if (args.Length == 0) ServerInfo = "127.0.0.1:20000";
+            else ServerInfo = args[0];
+
+            string ServerIP = ServerInfo.Split(':').First();
+            int ServerPort = int.Parse(ServerInfo.Split(':').Last());
+            TCPConnection connection = TCPConnection.GetConnection(new ConnectionInfo(ServerIP, 20000));
+
+            // Loop forever taking commands from the user.
+            //
+            while (true)
+            {
+                Console.WriteLine("Please choose an action:\n get - get a single entry\n add - add an entry\n bal - check the balance\n all - see all entries\n exit - exit the program");
+                string command = Console.ReadLine();
+                Console.Clear();
+
+                // Send a message requesting a single entry.
+                //
+                if (command == "get")
+                {
+                    Console.WriteLine("Enter the tag for the entry: ");
+                    string message = Console.ReadLine();
+
+                    float flt = connection.SendReceiveObject<float>("Get", "item", 2000, message);
+                    if (flt != 0.0) Console.WriteLine("$" + flt + "\n");
+                    else Console.WriteLine("Error: entry was not found\n");
+                }
+                // Send a message requesting a new entry be added.
+                //
+                else if (command == "add")
+                {
+                    Console.WriteLine("Type the new entry (type it as \"tag:amount\" (positive numbers for deposits and negative for withdrawls))");
+                    string message = Console.ReadLine();
+                    connection.SendObject("Add", message);
+                    Console.WriteLine("");
+                }
+                // Send a message requesting the balance.
+                //
+                else if (command == "bal")
+                {
+                    float flt = connection.SendReceiveObject<float>("Bal", "balance", 2000, "foo");
+                    Console.WriteLine("Your balance: $" + flt + "\n");
+                }
+                // Send a message requesting all transactions made until now.
+                //
+                else if (command == "all")
+                {
+                    Dictionary<string, float> book = new Dictionary<string, float>();
+                    book = connection.SendReceiveObject<Dictionary<string, float>>("All", "all", 2000, "foo");
+                    foreach (string temp in book.Keys)
+                    {
+                        Console.WriteLine(temp + ":" + book[temp]);
+                    }
+                    Console.WriteLine("");
+                }
+                // Quit if the user enters 'exit'.
+                //
+                else if (command == "exit") break;
+                // In case the user enters a bad command.
+                //
+                else
+                {
+                    Console.WriteLine("Sorry, I didn't recognize that.\n");
+                    continue;
+                }
+            }
+
+            // Shutdown the connection when we're done.
+            //
+            NetworkComms.Shutdown();
+        }
+    }
+}
